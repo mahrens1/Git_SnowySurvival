10ed485bc27d207eb83451d2034955952e94923b
mahrens1 <61665414+mahrens1@users.noreply.github.com>
2020-03-08 13:28:07 -0500

    initial commit of unity project

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject creditMenu;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void ReturnToMenu()
+    {
+        mainMenu.SetActive(true);
+        creditMenu.SetActive(false);
+    }
+
+    public void ShowCredits()
+    {
+        mainMenu.SetActive(false);
+        creditMenu.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Applicatoin.Quit();
+#endif
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    public float speed;
+    private bool isMoving = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S) && isMoving == false)
+        {
+            playerRB.AddForce(Vector3.forward * speed, ForceMode.Impulse);
+            isMoving = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.A) && isMoving == false)
+        {
+            playerRB.AddForce(Vector3.right * speed, ForceMode.Impulse);
+            isMoving = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.W) && isMoving == false)
+        {
+            playerRB.AddForce(Vector3.back * speed, ForceMode.Impulse);
+            isMoving = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && isMoving == false)
+        {
+            playerRB.AddForce(Vector3.left * speed, ForceMode.Impulse);
+            isMoving = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            playerRB.velocity = Vector3.zero;
+            isMoving = false;
+        }
+    }
+}
