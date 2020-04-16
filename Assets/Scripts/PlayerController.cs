35e9b2708021c453307b52fa59c5dfaae862356a
mahrens1 <61665414+mahrens1@users.noreply.github.com>
2020-04-16 18:16:03 -0500

    Added Ray Tracing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,25 +19,28 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         bool wasHitNorth = Physics.Raycast(transform.position, new Vector3(0, 0, -1), 2);
+        bool wasHitEast = Physics.Raycast(transform.position, new Vector3(-1, 0, 0), 2);
+        bool wasHitWest = Physics.Raycast(transform.position, new Vector3(1, 0, -1), 2);
+        bool wasHitSouth = Physics.Raycast(transform.position, new Vector3(0, 0, 1), 2);
         Color rayColor = (wasHitNorth) ? Color.red : Color.blue; 
         Debug.DrawRay(transform.position, new Vector3(0, 0, -2), rayColor, 1f);
 
-        if (Input.GetKeyDown(KeyCode.S) && isMoving == false)
+        if (Input.GetKeyDown(KeyCode.S) && wasHitSouth == false)
         {
             playerRB.AddForce(Vector3.forward * speed, ForceMode.Impulse);
             isMoving = true;
         }
-        else if(Input.GetKeyDown(KeyCode.A) && isMoving == false)
+        else if(Input.GetKeyDown(KeyCode.A) && wasHitWest == false)
         {
             playerRB.AddForce(Vector3.right * speed, ForceMode.Impulse);
             isMoving = true;
         }
-        else if (Input.GetKeyDown(KeyCode.W) && isMoving == false)
+        else if (Input.GetKeyDown(KeyCode.W) && wasHitNorth == false)
         {
             playerRB.AddForce(Vector3.back * speed, ForceMode.Impulse);
             isMoving = true;
         }
-        else if (Input.GetKeyDown(KeyCode.D) && isMoving == false)
+        else if (Input.GetKeyDown(KeyCode.D) && wasHitEast == false)
         {
             playerRB.AddForce(Vector3.left * speed, ForceMode.Impulse);
             isMoving = true;
