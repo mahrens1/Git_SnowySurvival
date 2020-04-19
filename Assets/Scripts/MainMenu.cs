aaf6dc3da0dd53952b2c5fb100ea96e09f9a30a0
mahrens1 <61665414+mahrens1@users.noreply.github.com>
2020-04-19 13:41:14 -0500

    Changed application quitter

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -28,10 +28,10 @@ public class MainMenu : MonoBehaviour
 
     public void QuitGame()
     {
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#else
+//#if UNITY_EDITOR
+        //UnityEditor.EditorApplication.isPlaying = false;
+//#else
         Application.Quit();
-#endif
+//#endif
     }
 }
