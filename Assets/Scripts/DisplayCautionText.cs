dca9d9bb2adf3543cf886743f321039532b4edec
mahrens1 <61665414+mahrens1@users.noreply.github.com>
2020-05-15 14:36:26 -0500

    Display text disabler

---
+++ Assets/Scripts/DisplayCautionText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DisplayCautionText : MonoBehaviour
+{
+
+    [SerializeField] GameObject cautionCanvas;
+    private float waitTime = 10f;
+    Scene currentScene;
+    string currentSceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentScene = SceneManager.GetActiveScene();
+        currentSceneName = currentScene.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentSceneName == "Level2")
+        {
+            Invoke("DisableCanvas", waitTime);
+        }
+    }
+
+    void DisableCanvas()
+    {
+        cautionCanvas.SetActive(false);
+    }
+}
