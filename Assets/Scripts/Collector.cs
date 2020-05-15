ac12a3b06447c46e9c79174635db993680afba7d
mahrens1 <61665414+mahrens1@users.noreply.github.com>
2020-05-15 12:14:27 -0500

    Homebase and collectable prefab
    added them to all levels

--- Assets/Scripts/Collector.cs
+++ Assets/Scripts/Collector.cs
@@ -34,15 +34,20 @@ public class Collector : MonoBehaviour
             hasCollectable = true;
         }
 
+
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
         //Scene changers
-        if (other.gameObject.CompareTag("HomeBase") && hasCollectable == true && sceneName == "Tutorial")
+        if (collision.gameObject.CompareTag("HomeBase") && hasCollectable == true && sceneName == "Tutorial")
         {
             SceneManager.LoadScene("Level2");
         }
-        if (other.gameObject.CompareTag("HomeBase") && hasCollectable == true && sceneName == "Level2")
+        if (collision.gameObject.CompareTag("HomeBase") && hasCollectable == true && sceneName == "Level2")
         {
             SceneManager.LoadScene("MainMenu");
-        }
-
+        } 
     }
 }
